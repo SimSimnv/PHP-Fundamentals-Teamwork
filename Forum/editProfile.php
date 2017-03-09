e340fd75d5f705962c1df68613f580a78a898bc4
nstoichkov7 <nstoichkov@gmail.com>
2017-03-09 20:56:09 +0200

    Add files via upload

---
+++ Forum/editProfile.php
@@ -0,0 +1,43 @@
+<?php
+include 'app.php';
+
+if($sessionService->isLogged() != true){
+    header("Location: home.php");
+    exit;
+}
+
+
+
+
+if(isset($_POST['update'])
+    && !empty($_POST['username'])
+    && !empty($_POST['email'])
+    && !empty($_POST['password']))
+{
+    $userid = $sessionService->getUserId();
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $forumService = new \ForumServices\MainService\ForumService($db,$encryptionService,$sessionService);
+    $forumService->editProfile($userid, $username, $email, $password);
+    $viewsRenderer->renderView("editProfile_OK_view");
+}
+
+
+if(isset($_POST['changePassword'])
+    && !empty($_POST['oldpassword'])
+    && !empty($_POST['newpassword'])
+    && !empty($_POST['confirm']))
+{
+    $userid = $sessionService->getUserId();
+    $oldpassword = $_POST['oldpassword'];
+    $newpassword = $_POST['newpassword'];
+    $confirm = $_POST['confirm'];
+
+    $forumService = new \ForumServices\MainService\ForumService($db,$encryptionService,$sessionService);
+    $forumService->changePassword($userid, $oldpassword, $newpassword, $confirm);
+    $viewsRenderer->renderView("editProfile_OK_view");
+}
+
+$viewsRenderer->renderView("editProfile_view");
