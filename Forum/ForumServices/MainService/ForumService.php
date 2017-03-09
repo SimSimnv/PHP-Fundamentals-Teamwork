b11539093dc4b4b65905904d38a9ca966a60fd9f
nstoichkov7 <nstoichkov@gmail.com>
2017-03-09 20:53:40 +0200

    Update ForumService.php
    Added changePassword and editProfile functions

--- Forum/ForumServices/MainService/ForumService.php
+++ Forum/ForumServices/MainService/ForumService.php
@@ -79,6 +79,79 @@ class ForumService implements ForumServiceInterface
         $this->sessionService->setUser($user->getId(),$user->getUsername());
     }
 
+    public function editProfile (int $id, string $username, string $email, string $password)
+   {
+       $loginQuery="SELECT id,password,username FROM users WHERE id=?";
+       $stmt=$this->db->prepare($loginQuery);
+       $stmt->execute([$id]);
+       $user=$stmt->fetchObject(User::class);
+//check for valid current password
+       $currnetPassword=$user->getPassword();
+       $passwordMatch=$this->encryptionService->isValid($currnetPassword,$password);
 
+       if(!$passwordMatch){
+           throw new \Exception("Incorrect password!");
+       }
+
+//update user details
+        $updateQuery="UPDATE
+                        `users`
+                    SET
+                        `username` = ?,
+                        `email`=?
+                    WHERE
+                        `id`=?";
+
+        $stmt=$this->db->prepare($updateQuery);
+        $result=$stmt->execute([$username,$email,$id]);
+
+        if($result===false){
+            //TODO check what caused failure ->username taken or password taken
+            throw new \Exception("Couldn't update your data!");
+        }
+
+   }
+
+    public function changePassword (int $id, string $oldpassword, string $newpassword, string $confirm)
+    {
+        // check for new password match
+        if($newpassword!=$confirm){
+            throw new \Exception("New Passwords don't match!");
+        }
+        $loginQuery="SELECT id,password,username FROM users WHERE id=?";
+        $stmt=$this->db->prepare($loginQuery);
+        $stmt->execute([$id]);
+        $user=$stmt->fetchObject(User::class);
+//check for valid current password
+        $currnetPassword=$user->getPassword();
+        $passwordMatch=$this->encryptionService->isValid($currnetPassword,$oldpassword);
+
+        if(!$passwordMatch){
+            throw new \Exception("Incorrect old password!");
+        }
+
+        if($newpassword!=$confirm){
+            throw new \Exception("New Passwords don't match!");
+        }
+
+        $hashNewPassword=$this->encryptionService->encrypt($newpassword);
+
+//update user details
+        $updateQuery="UPDATE
+                        `users`
+                    SET
+                        `password`=?
+                    WHERE
+                        `id`=?";
+
+        $stmt=$this->db->prepare($updateQuery);
+        $result=$stmt->execute([$hashNewPassword,$id]);
+
+        if($result===false){
+            //TODO check what caused failure ->username taken or password taken
+            throw new \Exception("Couldn't update your password!");
+        }
+
+    }
 
 }
