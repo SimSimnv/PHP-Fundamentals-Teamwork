dab26e31404392566c9c10dec8b7dc1a46f29b50
nstoichkov7 <nstoichkov@gmail.com>
2017-03-09 20:54:51 +0200

    Update ForumServiceInterface.php
    added editProfile and changePassword functions

--- Forum/ForumServices/MainService/ForumServiceInterface.php
+++ Forum/ForumServices/MainService/ForumServiceInterface.php
@@ -8,4 +8,6 @@ interface ForumServiceInterface
 {
     public function register(string $username, string $email, string $password, string $confirm);
     public function login(string $username, string $password);
+    public function editProfile (int $id, string $username, string $email, string $password);
+    public function changePassword (int $id, string $oldpassword, string $newpassword, string $confirm);
 }
