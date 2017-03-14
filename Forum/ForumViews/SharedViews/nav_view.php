7cbbdaad56a8cc7946d7732fd1a3c59d90175702
Hristian233 <hristian996@abv.bg>
2017-03-14 19:45:41 +0200

    Add files via upload

--- Forum/ForumViews/SharedViews/nav_view.php
+++ Forum/ForumViews/SharedViews/nav_view.php
@@ -53,7 +53,8 @@
 <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
-        <form action="question.php" method="post">
+
+        <form action="question.php" >
             <div class="navbar-header page-scroll">
                 <input type='text' name='title' value='' placeholder="Search question..." class='auto'>
                 <input type="hidden" name="url" value="<?= basename($_SERVER['PHP_SELF']) ?>">
@@ -61,7 +62,7 @@
         </form>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <li>
                     <a href="home.php">Home</a>
@@ -89,6 +90,7 @@
                 <?php endif; ?>
             </ul>
         </div>
+
         <!-- /.navbar-collapse -->
     </div>
     <!-- /.container -->
